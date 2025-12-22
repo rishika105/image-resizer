b41f7256aecf12515973a452356764801f284037
Rishika Agarwal <rishikaagarwal2316@gmail.com>
2025-12-22 19:33:24 +0530

    lets see again

--- src/App.jsx
+++ src/App.jsx
@@ -238,7 +238,7 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-gray-900 text-white p-8">
-    <div>CI/CD WORKEDDDD</div>
+    <div className="italic">CI/CD WORKEDDDD</div>
       <div className="max-w-7xl mx-auto">
         <h1 className="text-4xl font-bold mb-2">Image Resizing</h1>
         <p className="text-gray-400 mb-8">
